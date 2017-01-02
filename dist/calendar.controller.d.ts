42c6dce71c300cb1f65a400ccb13a2e8fd22b41f
HsuanXyz <HsuanGM@gmail.com>
2017-01-02 20:21:17 +0800

    update README.md

--- dist/calendar.controller.d.ts
+++ dist/calendar.controller.d.ts
@@ -2,5 +2,5 @@ import { ModalController } from 'ionic-angular';
 export declare class CalendarController {
     modalCtrl: ModalController;
     constructor(modalCtrl: ModalController);
-    openCalendar(calendarOptions: any): any;
+    openCalendar(calendarOptions?: any): any;
 }
