39411797e80340a9f493dc0ec46ce4102c3d9806
HsuanXyz <HsuanGM@gmail.com>
2017-05-28 13:30:01 +0800

    regulate code style

--- src/calendar.module.ts
+++ src/calendar.module.ts
@@ -5,20 +5,18 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA }           from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CalendarController } from './calendar.controller';
-import {IonicModule} from "ionic-angular";
-import {CalendarWeekComponent} from "./components/calendar-week-component";
-import {CalendarComponent} from "./components/calendar-component";
-import {MonthComponent} from "./components/month-component";
-import {CalendarService} from "./services/calendar.service";
+import { IonicModule } from "ionic-angular";
+import { CalendarWeekComponent } from "./components/calendar-week-component";
+import { CalendarComponent } from "./components/calendar-component";
+import { MonthComponent } from "./components/month-component";
+import { CalendarService } from "./services/calendar.service";
 
 @NgModule({
-  imports:      [  IonicModule, CommonModule ],
-  declarations: [ CalendarComponent, CalendarWeekComponent, MonthComponent],
-  providers:    [ CalendarController, CalendarService ],
+  imports: [IonicModule, CommonModule],
+  declarations: [CalendarComponent, CalendarWeekComponent, MonthComponent],
+  providers: [CalendarController, CalendarService],
   exports:[],
-  entryComponents:    [ CalendarComponent,  ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ]
+  entryComponents: [CalendarComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CalendarModule { }
--- src/services/calendar.service.ts
+++ src/services/calendar.service.ts
@@ -2,121 +2,120 @@
  * Created by hsuanlee on 27/05/2017.
  */
 import { Injectable } from '@angular/core';
-import {CalendarOriginal, CalendarDay, CalendarMonth, CalendarOptions, SavedDatesCache, CalendarControllerOptions} from '../calendar.model'
+import { CalendarOriginal, CalendarDay, CalendarMonth, CalendarControllerOptions } from '../calendar.model'
 import * as moment from 'moment';
 
 
- @Injectable()
- export class CalendarService {
+@Injectable()
+export class CalendarService {
 
-     constructor() {
-     }
-
-
-     createOriginalCalendar(time: number): CalendarOriginal {
-
-         const date = new Date(time);
-         const year = date.getFullYear();
-         const month = date.getMonth();
-         const firstWeek = new Date(year,month,1).getDay();
-         const howManyDays = moment(time).daysInMonth();
-
-         return {
-             time:time,
-             date:new Date(time),
-             year:year,
-             month:month,
-             firstWeek:firstWeek,
-             howManyDays:howManyDays
-         }
-     }
-
-     findDayConfig(day: any, opt:CalendarControllerOptions): any {
-
-         if(opt.daysConfig.length <= 0) return null;
-         return opt.daysConfig.find((n) => day.isSame(n.date,'day'))
-     }
-
-     createCalendarDay (time: number, opt:CalendarControllerOptions): CalendarDay {
-         let _time = moment(time);
-         let isToday = moment().isSame(_time, 'days');
-         let dayConfig = this.findDayConfig(_time, opt);
-         let _rangeBeg = moment(opt.from).valueOf();
-         let _rangeEnd = moment(opt.to).valueOf();
-         let isBetween = true;
-         let disableWee = opt.disableWeekdays.indexOf(_time.toDate().getDay()) !== -1;
-         if(_rangeBeg > 0 && _rangeEnd > 0){
-             if (!opt.canBackwardsSelected ){
-                 isBetween = !_time.isBetween(_rangeBeg, _rangeEnd,'days','[]');
-             }else {
-                 isBetween = moment(_time).isBefore(_rangeBeg) ? false : isBetween;
-             }
-         }else if (_rangeBeg > 0 && _rangeEnd === 0){
+    constructor() {
+    }
 
 
-             if (!opt.canBackwardsSelected ){
-                 let _addTime = _time.add('day',1);
-                 isBetween = !_addTime.isAfter(_rangeBeg);
-             }else {
-                 isBetween = false;
-             }
-         }
+    createOriginalCalendar(time: number): CalendarOriginal {
 
-         let _disable = disableWee || isBetween;
+        const date = new Date(time);
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const firstWeek = new Date(year,month,1).getDay();
+        const howManyDays = moment(time).daysInMonth();
 
-         return {
-             time: time,
-             isToday: isToday,
-             selected: false,
-             marked: dayConfig ? dayConfig.marked || false : false,
-             cssClass: dayConfig ? dayConfig.cssClass || '' : '',
-             disable: dayConfig ? dayConfig.disable || _disable : _disable,
-             title : dayConfig ? dayConfig.title || new Date(time).getDate().toString() : new Date(time).getDate().toString(),
-             subTitle: dayConfig ? dayConfig.subTitle || '' : ''
-         }
-     }
+        return {
+            time:time,
+            date:new Date(time),
+            year:year,
+            month:month,
+            firstWeek:firstWeek,
+            howManyDays:howManyDays,
+        }
+    }
 
-     createCalendarMonth(original: CalendarOriginal, opt:CalendarControllerOptions): CalendarMonth {
-         let days:Array<CalendarDay> = new Array(6).fill(null);
-         let len = original.howManyDays;
+    findDayConfig(day: any, opt:CalendarControllerOptions): any {
 
-         for(let i = original.firstWeek ; i < len+original.firstWeek; i++){
-             let itemTime = new Date(original.year,original.month,i - original.firstWeek+1).getTime();
-             days[i] = this.createCalendarDay(itemTime, opt);
-         }
+        if(opt.daysConfig.length <= 0) return null;
+        return opt.daysConfig.find((n) => day.isSame(n.date,'day'))
+    }
 
-         let weekStartDay = opt.weekStartDay;
+    createCalendarDay (time: number, opt:CalendarControllerOptions): CalendarDay {
+        let _time = moment(time);
+        let isToday = moment().isSame(_time, 'days');
+        let dayConfig = this.findDayConfig(_time, opt);
+        let _rangeBeg = moment(opt.from).valueOf();
+        let _rangeEnd = moment(opt.to).valueOf();
+        let isBetween = true;
+        let disableWee = opt.disableWeekdays.indexOf(_time.toDate().getDay()) !== -1;
+        if(_rangeBeg > 0 && _rangeEnd > 0){
+            if (!opt.canBackwardsSelected ){
+                isBetween = !_time.isBetween(_rangeBeg, _rangeEnd,'days','[]');
+            }else {
+                isBetween = moment(_time).isBefore(_rangeBeg) ? false : isBetween;
+            }
+        }else if (_rangeBeg > 0 && _rangeEnd === 0){
 
-         if(weekStartDay === 1){
-             if(days[0] === null){
-                 days.shift();
-                 days.push(...new Array(1).fill(null));
-             }else {
-                 days.unshift(null);
-                 days.pop();
-             }
-         }
 
-         return {
-             original: original,
-             days: days
-         }
+            if (!opt.canBackwardsSelected ){
+                let _addTime = _time.add('day',1);
+                isBetween = !_addTime.isAfter(_rangeBeg);
+            }else {
+                isBetween = false;
+            }
+        }
 
-     }
+        let _disable = disableWee || isBetween;
 
-     createMonthsByPeriod(startTime: number, monthsNum: number, opt:CalendarControllerOptions): Array<CalendarMonth> {
-         let _array:Array<CalendarMonth> = [];
+        return {
+            time: time,
+            isToday: isToday,
+            selected: false,
+            marked: dayConfig ? dayConfig.marked || false : false,
+            cssClass: dayConfig ? dayConfig.cssClass || '' : '',
+            disable: dayConfig ? dayConfig.disable || _disable : _disable,
+            title : dayConfig ? dayConfig.title || new Date(time).getDate().toString() : new Date(time).getDate().toString(),
+            subTitle: dayConfig ? dayConfig.subTitle || '' : '',
+        }
+    }
 
-         let _start = new Date(startTime);
-         let _startMonth = new Date(_start.getFullYear(),_start.getMonth(),1).getTime();
+    createCalendarMonth(original: CalendarOriginal, opt:CalendarControllerOptions): CalendarMonth {
+        let days:Array<CalendarDay> = new Array(6).fill(null);
+        let len = original.howManyDays;
 
-         for(let i = 0; i < monthsNum; i++ ){
-             let time = moment(_startMonth).add(i,'M').valueOf();
-             let originalCalendar = this.createOriginalCalendar(time);
-             _array.push(this.createCalendarMonth(originalCalendar, opt))
-         }
+        for(let i = original.firstWeek ; i < len+original.firstWeek; i++){
+            let itemTime = new Date(original.year,original.month,i - original.firstWeek+1).getTime();
+            days[i] = this.createCalendarDay(itemTime, opt);
+        }
 
-         return _array;
-     }
+        let weekStartDay = opt.weekStartDay;
 
- }
+        if(weekStartDay === 1){
+            if(days[0] === null){
+                days.shift();
+                days.push(...new Array(1).fill(null));
+            }else {
+                days.unshift(null);
+                days.pop();
+            }
+        }
+
+        return {
+            original: original,
+            days: days,
+        }
+
+    }
+
+    createMonthsByPeriod(startTime: number, monthsNum: number, opt:CalendarControllerOptions): Array<CalendarMonth> {
+        let _array:Array<CalendarMonth> = [];
+
+        let _start = new Date(startTime);
+        let _startMonth = new Date(_start.getFullYear(),_start.getMonth(),1).getTime();
+
+        for(let i = 0; i < monthsNum; i++ ){
+            let time = moment(_startMonth).add(i,'M').valueOf();
+            let originalCalendar = this.createOriginalCalendar(time);
+            _array.push(this.createCalendarMonth(originalCalendar, opt))
+        }
+
+        return _array;
+    }
+}
