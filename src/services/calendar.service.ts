d56e4f4f991c5520f60af88368dcb0657dcbb8ec
HsuanXyz <HsuanGM@gmail.com>
2017-07-04 20:50:53 +0800

    add safeOpt

--- src/services/calendar.service.ts
+++ src/services/calendar.service.ts
@@ -13,6 +13,55 @@ export class CalendarService {
 
     }
 
+    safeOpt(calendarOptions: CalendarControllerOptions) {
+        let _arr:Array<any> = [];
+
+        let {
+            from = new Date(),
+            to = 0,
+            cssClass = '',
+            weekStartDay = 0,
+            isRadio = true,
+            canBackwardsSelected = false,
+            disableWeekdays = _arr,
+            closeLabel = 'cancel',
+            closeIcon = false,
+            id = '',
+            color = 'primary',
+            isSaveHistory = false,
+            monthTitle = 'MMM yyyy',
+            title = 'Calendar',
+            weekdaysTitle = "Di_Lu_Ma_Me_Je_Ve_Sa".split("_"),
+            daysConfig = _arr,
+            countNextMonths = 3,
+            showYearPicker = false,
+        } = calendarOptions || {};
+
+        let options: CalendarControllerOptions = {
+            from:from,
+            to:to,
+            cssClass:cssClass,
+            isRadio:isRadio,
+            weekStartDay:weekStartDay,
+            canBackwardsSelected:canBackwardsSelected,
+            closeLabel:closeLabel,
+            closeIcon:closeIcon,
+            id:id,
+            color:color,
+            isSaveHistory:isSaveHistory,
+            defaultDate:calendarOptions.defaultDate || from ,
+            disableWeekdays:disableWeekdays,
+            monthTitle:monthTitle,
+            title:title,
+            weekdaysTitle:weekdaysTitle,
+            daysConfig:daysConfig,
+            countNextMonths:countNextMonths,
+            showYearPicker:showYearPicker,
+        };
+
+        return options
+    }
+
     createOriginalCalendar(time: number): CalendarOriginal {
 
         const date = new Date(time);
