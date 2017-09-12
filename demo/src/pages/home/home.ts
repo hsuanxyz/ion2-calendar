0d62052ec17b15bab6540de1d596bb9b00afb034
Hsuan Lee <HsuanGM@gmail.com>
2017-09-12 20:08:16 +0800

    🎉 release: ion2-calendar@2.0.0

--- demo/src/pages/home/home.ts
+++ demo/src/pages/home/home.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ModalController, NavController } from 'ionic-angular';
 
-import { CalendarComponentOptions, CalendarController, DayConfig } from 'ion2-calendar'
+import {
+  CalendarComponentOptions,
+  CalendarModalOptions,
+  CalendarModal,
+  DayConfig
+} from 'ion2-calendar'
 
+import * as moment from 'moment'
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -12,7 +18,7 @@ export class HomePage {
   days: Array<any> = [];
   date: string;
   dateMulti = [];
-  dateRangeObj: any;
+  dateRangeObj: { from: string; to: string; };
   format = 'YYYY-MM-DD';
   optionsMulti: CalendarComponentOptions = {
     pickMode: 'multi'
@@ -24,174 +30,188 @@ export class HomePage {
   };
 
   constructor(public navCtrl: NavController,
-              public calendarCtrl: CalendarController,) {
+              public modalCtrl: ModalController) {
 
   }
 
-  basic() {
+  onChange($event) {
+    console.log($event);
+  }
 
-    this.calendarCtrl.openCalendar({
+  basic() {
+    const options = {
       title: 'BASIC',
       canBackwardsSelected: true,
       color: 'cal-color',
       doneIcon: true,
       closeIcon: true
-    })
-    .then((res: any) => {
-      console.log(res)
-    })
-    .catch(() => {
+    };
+    let myCalendar =  this.modalCtrl.create(CalendarModal, {
+      options: options
     });
+
+    myCalendar.present();
+
+    myCalendar.onDidDismiss(date => {
+      console.log(date);
+    })
   }
 
   multi() {
-    this.calendarCtrl.openCalendar({
+    const options = {
       pickMode: 'multi',
       title: 'MULTI',
-      defaultDates: [new Date(2017, 7, 20), new Date(2017, 7, 18).getTime()]
-    })
-    .then((res: any) => {
-      console.log(res)
-    })
-    .catch(() => {
+      defaultDates: [moment(), moment().add(1, 'd'), moment().add(2, 'd')]
+    };
+
+    let myCalendar =  this.modalCtrl.create(CalendarModal, {
+      options: options
     });
-  }
 
-  setDefaultDate() {
-    this.calendarCtrl.openCalendar({
-      from: new Date(2017, 1, 1),
-      defaultScrollTo: new Date(2017, 4, 1),
+    myCalendar.present();
 
-    })
-    .then((res: any) => {
-      console.log(res)
-    })
-    .catch(() => {
-    })
-  }
-
-
-  setCssClass() {
-    this.calendarCtrl.openCalendar({
-      cssClass: 'my-class',
-      color: 'secondary',
-      pickMode: 'range',
-      autoDone: true
-    })
-    .then((res: any) => {
-      console.log(res)
-    })
-    .catch(() => {
+    myCalendar.onDidDismiss(date => {
+      console.log(date);
     })
   }
 
   dateRange() {
-    this.calendarCtrl.openCalendar({
+    const options: CalendarModalOptions = {
       pickMode: 'range',
       title: 'RANGE',
       canBackwardsSelected: true,
       color: 'danger'
-    })
-    .then((res: any) => {
-      console.log(res)
-    })
-    .catch(() => {
-    })
+    };
+
+    let myCalendar =  this.modalCtrl.create(CalendarModal, {
+      options: options
+    });
+
+    myCalendar.present();
+
+    myCalendar.onDidDismiss(date => {
+      console.log(date);
+    });
+  }
+
+  setDefaultDate() {
+    const options: CalendarModalOptions = {
+      from: new Date(2017, 1, 1),
+      defaultScrollTo: new Date(2017, 4, 1),
+      defaultDate:  new Date(2017, 4, 1)
+    };
+
+    let myCalendar =  this.modalCtrl.create(CalendarModal, {
+      options: options
+    });
+
+    myCalendar.present();
+
+    myCalendar.onDidDismiss(date => {
+      console.log(date);
+    });
+
+  }
+
+  setCssClass() {
+    const options: CalendarModalOptions = {
+      cssClass: 'my-class',
+      color: 'secondary',
+      pickMode: 'range',
+      autoDone: true
+    };
+
+    let myCalendar =  this.modalCtrl.create(CalendarModal, {
+      options: options
+    });
+
+    myCalendar.present();
+
+    myCalendar.onDidDismiss(date => {
+      console.log(date);
+    });
   }
 
   optional() {
-    this.calendarCtrl.openCalendar({
+    const options: CalendarModalOptions = {
       from: new Date(2017, 1, 1),
       to: new Date(2017, 2, 5),
-    })
-    .then((res: any) => {
-      console.log(res)
-    })
-    .catch(() => {
-    })
+    };
+
+    let myCalendar =  this.modalCtrl.create(CalendarModal, {
+      options: options
+    });
+
+    myCalendar.present();
+
+    myCalendar.onDidDismiss(date => {
+      console.log(date);
+    });
   }
 
   disableWeekdays() {
-    this.calendarCtrl.openCalendar({
+    const options: CalendarModalOptions = {
       disableWeeks: [0, 6],
       canBackwardsSelected: true,
-    })
-    .then((res: any) => {
-      console.log(res)
-    })
-    .catch(() => {
-    })
+    };
+
+    let myCalendar =  this.modalCtrl.create(CalendarModal, {
+      options: options
+    });
+
+    myCalendar.present();
+
+    myCalendar.onDidDismiss(date => {
+      console.log(date);
+    });
   }
 
   local() {
-
-    this.calendarCtrl.openCalendar({
+    const options: CalendarModalOptions = {
       monthFormat: 'yyyy 年 MM 月 ',
       weekdays: ['天', '一', '二', '三', '四', '五', '六'],
       weekStart: 1,
       color: 'light',
       defaultDate: new Date()
-    })
-    .then((res: any) => {
-      console.log(res)
-    })
-    .catch(() => {
-    })
+    };
+
+    let myCalendar =  this.modalCtrl.create(CalendarModal, {
+      options: options
+    });
+
+    myCalendar.present();
+
+    myCalendar.onDidDismiss(date => {
+      console.log(date);
+    });
   }
 
   daysConfig() {
-
-    let _daysConfig: DayConfig[] = [
-      {
-        date: new Date(2017, 0, 1),
-        subTitle: 'New Year\'s',
-        marked: true,
-        cssClass: 'day-danger',
-      },
-      {
-        date: new Date(2017, 1, 14),
-        subTitle: 'Valentine\'s',
-      },
-      {
-        date: new Date(2017, 3, 1),
-        subTitle: 'April Fools',
-        marked: true
-      },
-      {
-        date: new Date(2017, 3, 7),
-        subTitle: 'World Health',
-      },
-      {
-        date: new Date(2017, 4, 31),
-        subTitle: 'No-Smoking',
-      },
-      {
-        date: new Date(2017, 5, 1),
-        subTitle: 'Children\'s',
-      }
-    ];
-
+    let _daysConfig: DayConfig[] = [];
     for (let i = 0; i < 31; i++) {
-      this.days.push({
+      _daysConfig.push({
         date: new Date(2017, 0, i + 1),
         subTitle: `$${i + 1}`
       })
     }
 
-    _daysConfig.push(...this.days);
-
-    this.calendarCtrl.openCalendar({
+    const options: CalendarModalOptions = {
       from: new Date(2017, 0, 1),
       to: new Date(2017, 11.1),
       daysConfig: _daysConfig,
       cssClass: 'my-cal',
-    })
-    .then((res: any) => {
-      console.log(res)
-    })
+    };
 
-    .catch(() => {
-    })
+    let myCalendar =  this.modalCtrl.create(CalendarModal, {
+      options: options
+    });
+
+    myCalendar.present();
+
+    myCalendar.onDidDismiss(date => {
+      console.log(date);
+    });
   }
+
 
 }
