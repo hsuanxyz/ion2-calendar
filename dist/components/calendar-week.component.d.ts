08511bb398e9262407e0a51030ea857e73007c8f
Hsuan Lee <HsuanGM@gmail.com>
2018-01-03 20:07:15 +0800

    chore: remove dist from git repository

--- dist/components/calendar-week.component.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CalendarWeekComponent {
-    _weekArray: string[];
-    _weekStart: number;
-    color: string;
-    constructor();
-    weekArray: string[];
-    weekStart: number;
-    adjustSort(): void;
-}
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-export * from './calendar.model';
-export { CalendarModal } from "./components/calendar.modal";
-export { CalendarWeekComponent } from "./components/calendar-week.component";
-export { MonthComponent } from "./components/month.component";
-export { CalendarComponent } from "./components/calendar.component";
-export { CalendarModule } from './calendar.module';
-export { CalendarController } from './calendar.controller';
-//# sourceMappingURL=index.js.map
