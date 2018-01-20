83a6fd4d69af727e0607a1cb0993ff63fa63dc7f
Hsuan Lee <HsuanGM@gmail.com>
2018-01-20 20:07:39 +0800

    chore: change build stripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp');
 
-var TEMP_PATH = './.tmp';
-var DIST_PATH = './dist';
-var SRC_PATH = './src';
+const paths = {
+  dist: './dist',
+  dve: './dev/src/ion2-calendar',
+  src: './src'
+};
 
-gulp.task('src2temp', function() {
-    gulp.src( SRC_PATH + '/**/**').pipe(gulp.dest(TEMP_PATH));
-});
 
-gulp.task('copy-scss', function() {
-    gulp.src( SRC_PATH + '/**/*.scss').pipe(gulp.dest(DIST_PATH));
-});
+gulp.task('copy-sources', copySources);
+gulp.task('copy-scss', copyScss);
 
-gulp.task('dist2nm', function() {
-    gulp.src( DIST_PATH+'/**/**').pipe(gulp.dest('demo/node_modules/ion2-calendar'));
-});
+function copySources() {
+    gulp.src(`${paths.dve}/**/*`)
+    .pipe(gulp.dest(paths.src))
+}
+
+function copyScss() {
+    gulp.src(`${paths.src}/**/*.scss`).pipe(gulp.dest(paths.dist));
+}
