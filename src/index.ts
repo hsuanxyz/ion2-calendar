49b5a8a209d4985d27f3982e2de1d109f64c64b4
DanilF <danil_flores@ultimatesoftware.com>
2019-02-27 17:26:53 -0500

    feat: add ability to set global defaults

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export * from './calendar.model';
-export { CalendarModal } from "./components/calendar.modal";
-export { CalendarWeekComponent } from "./components/calendar-week.component";
-export { MonthComponent } from "./components/month.component";
-export { CalendarComponent } from "./components/calendar.component";
+export { CalendarModal } from './components/calendar.modal';
+export { CalendarWeekComponent } from './components/calendar-week.component';
+export { MonthComponent } from './components/month.component';
+export { DEFAULT_CALENDAR_OPTIONS } from './services/calendar-options.provider';
+export { CalendarComponent } from './components/calendar.component';
 export { CalendarModule } from './calendar.module';
 export { CalendarController } from './calendar.controller';
