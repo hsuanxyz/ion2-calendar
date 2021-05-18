daf6e8f25cc95a2a602ee08766279a2947c5e088
Cosme Nieto <cosme.nieto@azulae.es>
2021-05-18 13:45:16 +0200

    demo: add options to see week view related configurations

--- demo/src/demos/demo-options.ts
+++ demo/src/demos/demo-options.ts
@@ -23,7 +23,7 @@ import {
       </ion-item>
       <ion-item>
         <ion-label>disableWeeks</ion-label>
-        <ion-select [(ngModel)]="_disableWeeks" 
+        <ion-select [(ngModel)]="_disableWeeks"
                     (ngModelChange)="_changeDisableWeeks($event)"
                     multiple="true">
           <ion-option value="0">0</ion-option>
@@ -50,6 +50,34 @@ import {
         <ion-label>showMonthPicker</ion-label>
         <ion-checkbox [(ngModel)]="_showMonthPicker" (ngModelChange)="_changeShowMonthPicker($event)"></ion-checkbox>
       </ion-item>
+      <ion-item>
+        <ion-label>showAdjacentMonthDay</ion-label>
+        <ion-checkbox [(ngModel)]="_showAdjacentMonthDay" (ngModelChange)="_changeShowAdjacentMonthDay($event)"></ion-checkbox>
+      </ion-item>
+      <ion-item>
+        <ion-label>displayMode</ion-label>
+        <ion-radio-group [(ngModel)]="_displayMode" (ngModelChange)="_changeDisplayMode($event)">
+          <ion-row>
+            <ion-item lines="none">
+              <ion-label>month &nbsp;</ion-label>
+              <ion-radio value="month"></ion-radio>
+            </ion-item>
+            <ion-item lines="none">
+              <ion-label>week &nbsp;</ion-label>
+              <ion-radio value="week"></ion-radio>
+            </ion-item>
+          </ion-row>
+        </ion-radio-group>
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">weeks</ion-label>
+        <ion-input [disabled]="_displayMode !== 'week'" type="number" inputmode="numeric" min="1" step="1" [(ngModel)]="_weeks"
+                   (ngModelChange)="_changeNumberWeeks($event)"></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label>continuous</ion-label>
+        <ion-checkbox [disabled]="_displayMode !== 'week'" [(ngModel)]="_continuous" (ngModelChange)="_changeContinuous($event)"></ion-checkbox>
+      </ion-item>
     </ion-list>
 
     <ion-calendar [(ngModel)]="date"
@@ -67,6 +95,10 @@ export class DemoOptionsComponent {
   _showMonthPicker: boolean = true;
   _disableWeeks: number[] = [0, 6];
   _weekStart: number = 0;
+  _weeks: number = 1;
+  _continuous: boolean = false;
+  _displayMode: string = 'month';
+  _showAdjacentMonthDay: boolean = true;
   date: string = '2018-01-01';
   options: CalendarComponentOptions = {
     from: new Date(2000, 0, 1),
@@ -113,6 +145,38 @@ export class DemoOptionsComponent {
     this.options = {
       ...this.options,
       weekStart: parseInt(weekStart)
-    };
+    }
+  }
+
+  _changeDisplayMode(displayMode: string) {
+    if (displayMode === 'week' || displayMode === 'month'){
+      this.options = {
+        ...this.options,
+        displayMode: displayMode
+      }
+    }
+  }
+
+  _changeNumberWeeks(weeks: number) {
+    if (weeks > 0){
+      this.options = {
+        ...this.options,
+        weeks: weeks
+      }
+    }
+  }
+
+  _changeContinuous(continuous: boolean) {
+    this.options = {
+      ...this.options,
+      continuous
+    }
+  }
+
+  _changeShowAdjacentMonthDay(showAdjacentMonthDay: boolean) {
+    this.options = {
+      ...this.options,
+      showAdjacentMonthDay
+    }
   }
 }
